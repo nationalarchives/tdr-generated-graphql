1094470f45c25d73c3de976fd895d1c95e1f0bfd
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-03-25 10:20:33 +0000

    More small fixes

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,4 +1,6 @@
-git checkout $BRANCH_NAME
+git config --global user.email digitalpreservation@nationalarchives.gov.uk
+git config --global user.name tna-digital-archiving-jenkins
+git checkout -b $BRANCH_NAME
 git push -u origin $BRANCH_NAME
 mkdir -p src/main/resources
 wget -O src/main/resources/schema.graphql https://raw.githubusercontent.com/nationalarchives/tdr-consignment-api/master/schema.graphql

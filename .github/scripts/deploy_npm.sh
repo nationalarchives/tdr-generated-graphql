d03fc89fcd65eabfc071882aa4ccef060b6486ab
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-05-06 09:50:42 +0100

    Split npm and maven deploy.
    We're deploying to npm and maven for any changes to this repository.
    This means that dependabot updates for package.json deploy a new version
    to maven which triggers a load of updates from ScalaSteward for this
    library.
    
    This should only deploy to maven if the changes aren't in the ts
    directory.

---
+++ .github/scripts/deploy_npm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+npm config set //registry.npmjs.org/:_authToken=$1
+cd ts
+npm ci
+npm run codegen
+npm run build
+npm version patch
+git add package.json package-lock.json
+git commit -m 'Update npm version'
+npm publish --access public
+cd ..

2b6d291bfa6ec550dfb2bf50212f513ed81c784a
thanhz <thanhz@hotmail.com>
2022-05-19 16:35:23 +0100

    uncomment pull request and deploy script

--- .github/scripts/deploy_npm.sh
+++ .github/scripts/deploy_npm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#npm config set //registry.npmjs.org/:_authToken=$1
-#cd ts
-#npm ci
-#npm run codegen
-#npm run build
-#npm version patch
-#git add package.json package-lock.json
-#git commit -m 'Update npm version'
-#git push
-#npm publish --access public
-#cd ..
+npm config set //registry.npmjs.org/:_authToken=$1
+cd ts
+npm ci
+npm run codegen
+npm run build
+npm version patch
+git add package.json package-lock.json
+git commit -m 'Update npm version'
+git push
+npm publish --access public
+cd ..
 echo ::set-output name=set-npm-version::$(awk '/version/{gsub(/("|",)/,"",$2);print $2}' ts/package.json)

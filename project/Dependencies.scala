30e8c50b3f919896663c1dc67bd60d5fca49046d
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-01-16 08:30:26 +0000

    Update sangria to 3.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.15"
-  lazy val sangria = "org.sangria-graphql" %% "sangria" % "3.4.1"
+  lazy val sangria = "org.sangria-graphql" %% "sangria" % "3.5.0"
   lazy val circeCore = "io.circe" %% "circe-core" % "0.14.3"
   lazy val circeGeneric = "io.circe" %% "circe-generic" % "0.14.3"
 }

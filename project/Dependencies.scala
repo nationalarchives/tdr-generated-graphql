ab72a882aa645a607da3b1b0b3d4410bafa74799
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-08-31 08:33:11 +0000

    Update circe-core, circe-generic to 0.14.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,6 +3,6 @@ import sbt._
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.16"
   lazy val sangria = "org.sangria-graphql" %% "sangria" % "4.0.1"
-  lazy val circeCore = "io.circe" %% "circe-core" % "0.14.5"
-  lazy val circeGeneric = "io.circe" %% "circe-generic" % "0.14.5"
+  lazy val circeCore = "io.circe" %% "circe-core" % "0.14.6"
+  lazy val circeGeneric = "io.circe" %% "circe-generic" % "0.14.6"
 }

194b2269ff9ba49558c47545125517c6386fbc0c
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-06-25 08:27:57 +0000

    Update scalatest to 3.2.19 in scala-steward-dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.18"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.19"
   lazy val sangria = "org.sangria-graphql" %% "sangria" % "4.1.0"
   lazy val circeCore = "io.circe" %% "circe-core" % "0.14.8"
   lazy val circeGeneric = "io.circe" %% "circe-generic" % "0.14.8"
